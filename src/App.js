707efe3eef4c077eab2340afd47c74b054c5873a
DEVESHDS <dev1710.sharma@gmail.com>
2021-03-18 14:45:22 +0530

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
 import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
 
 function App() {
-  return <div className="App">Let's Build the react app</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "./assets/images/download.jpg";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import LanguageIcon from "@material-ui/icons/Language";
+import { Avatar } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className="header">
+      <img className="header__icon" src={Logo} alt="airbnb logo" />
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
